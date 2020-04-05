cb034402f3d9ba3c8afac1aeed0fe705f6d69b71
NanairoMegane <kobarengaruda@gmail.com>
2020-04-06 00:58:50 +0900

    innerjoin, outerjoin

--- practical.go
+++ practical.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/volatiletech/null"
 	"github.com/volatiletech/sqlboiler/boil"
 	"github.com/volatiletech/sqlboiler/queries/qm"
 )
@@ -24,7 +25,19 @@ func practical_test() error {
 		return err
 	}
 
+	if err := raw_query(); err != nil {
+		return err
+	}
+
 	if err := custome_struct(); err != nil {
+		return err
+	}
+
+	if err := inner_join(); err != nil {
+		return err
+	}
+
+	if err := outer_join(); err != nil {
 		return err
 	}
 
@@ -78,6 +91,34 @@ func custome_query() error {
 	return err
 }
 
+func raw_query() error {
+
+	var result = new(UserSliceModel)
+
+	var query = `
+	SELECT 
+		user_id,
+		name,
+		age
+	FROM 
+		user
+	WHERE
+		age = 25
+	`
+
+	err := models.NewQuery(qm.SQL(query)).Bind(context.Background(), DB, &result.UserSlice)
+	if err == nil {
+		fmt.Println("--- raw query ---")
+		for _, user := range result.UserSlice {
+			fmt.Printf("result : \n  user_id: %d\n  name: %s\n  age: %d\n",
+				user.UserID, user.Name, user.Age)
+		}
+	}
+	fmt.Println()
+
+	return err
+}
+
 func custome_struct() error {
 
 	var result = new(UserCustomeSliceModel)
@@ -102,12 +143,57 @@ func custome_struct() error {
 	return err
 }
 
-func inner_join() {
+func inner_join() error {
 
+	var result = new(UserAndDivisionSliceModel)
+
+	var queries []qm.QueryMod
+	{
+		queries = append(queries, qm.Select("U.user_id AS user_id, U.name AS user_name, D.division_id AS division_id, D.name AS division_name"))
+		queries = append(queries, qm.From("User AS U"))
+		queries = append(queries, qm.InnerJoin("Division AS D ON U.user_id = D.user_id"))
+	}
+
+	err := models.NewQuery(queries...).Bind(context.Background(), DB, &result.UDSlice)
+	if err == nil {
+		fmt.Println("--- inner join ---")
+		for _, ud := range result.UDSlice {
+			fmt.Printf("result : \n  user_id: %d\n  user_name: %s\n  division_id: %d\n  division_name: %s\n",
+				ud.UserID, ud.UserName, ud.DivisionID, ud.DivisionName)
+		}
+	}
+	fmt.Println()
+
+	return err
 }
 
-func outer_join() {
+func outer_join() error {
+	var result = new(UserAndBranchSliceModel)
 
+	var outer_join_query = `
+		SELECT 
+			U.user_id AS user_id, 
+			U.name AS user_name, 
+			B.branch_id AS branch_id, 
+			B.name AS branch_name
+		FROM 
+			user AS U
+		LEFT OUTER JOIN 
+			Branch AS B 
+		ON 
+			U.user_id = B.user_id`
+
+	err := models.NewQuery(qm.SQL(outer_join_query)).Bind(context.Background(), DB, &result.UBSlice)
+	if err == nil {
+		fmt.Println("--- outer join ---")
+		for _, ub := range result.UBSlice {
+			fmt.Printf("result : \n  user_id: %d\n  user_name: %s\n  branch_id: %d\n  branch_name: %s\n",
+				ub.UserID, ub.UserName, ub.BranchID.Int, ub.BranchName.String)
+		}
+	}
+	fmt.Println()
+
+	return err
 }
 
 func prepare_data() error {
@@ -121,18 +207,25 @@ func prepare_data() error {
 	if err := (&models.User{UserID: 3, Name: "Sasa", Age: 27}).Insert(context.Background(), DB, boil.Infer()); err != nil {
 		return err
 	}
+	if err := (&models.Division{DivisionID: 1, Name: "Akita", UserID: 1}).Insert(context.Background(), DB, boil.Infer()); err != nil {
+		return err
+	}
+	if err := (&models.Branch{BranchID: 1, Name: "Oodate", UserID: null.IntFrom(1)}).Insert(context.Background(), DB, boil.Infer()); err != nil {
+		return err
+	}
 
 	return nil
 }
 
 func delete_data() error {
-	if _, err := models.Users().DeleteAll(context.Background(), DB); err != nil {
-		return err
-	}
+
 	if _, err := models.Divisions().DeleteAll(context.Background(), DB); err != nil {
 		return err
 	}
 	if _, err := models.Branches().DeleteAll(context.Background(), DB); err != nil {
+		return err
+	}
+	if _, err := models.Users().DeleteAll(context.Background(), DB); err != nil {
 		return err
 	}
 
--- structs.go
+++ structs.go
@@ -1,6 +1,10 @@
 package main
 
-import "NanairoMegane/sqlboiler_test/models"
+import (
+	"NanairoMegane/sqlboiler_test/models"
+
+	"github.com/volatiletech/null"
+)
 
 type UserModel struct {
 	models.User
@@ -17,4 +21,26 @@ type UserCustomeModel struct {
 
 type UserCustomeSliceModel struct {
 	USlice []*UserCustomeModel
+}
+
+type UserAndDivision struct {
+	UserID       int    `boil:"user_id"`
+	UserName     string `boil:"user_name"`
+	DivisionID   int    `boil:"division_id"`
+	DivisionName string `boil:"division_name"`
+}
+
+type UserAndDivisionSliceModel struct {
+	UDSlice []*UserAndDivision
+}
+
+type UserAndBranch struct {
+	UserID     int         `boil:"user_id"`
+	UserName   string      `boil:"user_name"`
+	BranchID   null.Int    `boil:"branch_id"`
+	BranchName null.String `boil:"branch_name"`
+}
+
+type UserAndBranchSliceModel struct {
+	UBSlice []*UserAndBranch
 }
